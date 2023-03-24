ccd4079275591b5942abdb4f5f7bed374cde4ebf
Srishti0901 <103064237+Srishti0901@users.noreply.github.com>
2023-03-25 00:41:56 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -2,4 +2,4 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const DB_url = "mongodb://0.0.0.0:27017/Ecom";
-mongoose.connect(DB_url, { useNewUrlParser: true }).then(() => console.log("Db connected")).catch(err => console.log(err.message));
+mongoose.connect("mongodb+srv://AtlasS:AtlasS123@cluster0.chu2hxx.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true }).then(() => console.log("Db connected")).catch(err => console.log(err.message));
